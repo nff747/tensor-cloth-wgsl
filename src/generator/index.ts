45e8b17044ca871c4e3fa0a1e05881ed75039bce
nff747 <nff747@users.noreply.github.com>
2026-09-23 20:43:58 -0300

    add Spring struct to WGSL generator

--- src/generator/index.ts
+++ src/generator/index.ts
@@ -9,7 +9,18 @@ struct Particle {
     `.trim();
   }
 
+  public generateSpringStruct(): string {
+    return `
+struct Spring {
+  p1: u32,
+  p2: u32,
+  rest_length: f32,
+  stiffness: f32,
+};
+    `.trim();
+  }
+
   public generate(): string {
-    return this.generateParticleStruct();
+    return this.generateParticleStruct() + "\n\n" + this.generateSpringStruct();
   }
 }
