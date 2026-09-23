5906427bdd61f5786645bfd5a8b179097252799f
nff747 <nff747@users.noreply.github.com>
2026-09-23 20:43:44 -0300

    add Particle struct to WGSL generator

--- src/generator/index.ts
+++ src/generator/index.ts
@@ -1,5 +1,15 @@
 export class WgslGenerator {
+  public generateParticleStruct(): string {
+    return `
+struct Particle {
+  position: vec3<f32>,
+  velocity: vec3<f32>,
+  mass: f32,
+};
+    `.trim();
+  }
+
   public generate(): string {
-    return "";
+    return this.generateParticleStruct();
   }
 }
