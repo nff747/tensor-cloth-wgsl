7587749eb7fd446ea6927e70ca36a029df0ba81c
nff747 <nff747@users.noreply.github.com>
2026-09-23 20:44:15 -0300

    add WGSL resource bindings

--- src/generator/index.ts
+++ src/generator/index.ts
@@ -30,11 +30,20 @@ struct Uniforms {
     `.trim();
   }
 
+  public generateBindings(): string {
+    return `
+@group(0) @binding(0) var<uniform> uniforms: Uniforms;
+@group(0) @binding(1) var<storage, read_write> particles: array<Particle>;
+@group(0) @binding(2) var<storage, read> springs: array<Spring>;
+    `.trim();
+  }
+
   public generate(): string {
     return [
       this.generateParticleStruct(),
       this.generateSpringStruct(),
-      this.generateUniformStruct()
+      this.generateUniformStruct(),
+      this.generateBindings()
     ].join("\n\n");
   }
 }
